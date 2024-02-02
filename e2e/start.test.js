1b11cfadc02d7604d6c19c9ed71a189566bdf6df
Kate <katewolshanat@gmail.com>
2024-02-02 13:22:44 +0100

    fixed eslint

--- e2e/start.test.js
+++ e2e/start.test.js
@@ -1,21 +1,20 @@
-
-import puppetteer from 'puppeteer';
-import { fork } from 'child_process';
+import puppetteer from "puppeteer";
+import { fork } from "child_process";
 
 jest.setTimeout(30000); // default puppeteer timeout
 
-describe('Credit Card Validator form', () => {
+describe("Credit Card Validator form", () => {
   let browser = null;
   let page = null;
   let server = null;
-  const baseUrl = 'http://localhost:8080';
+  const baseUrl = "http://localhost:8080";
 
   beforeAll(async () => {
     server = fork(`${__dirname}/e2e.server.js`);
     await new Promise((resolve, reject) => {
-      server.on('error', reject);
-      server.on('message', (message) => {
-        if (message === 'ok') {
+      server.on("error", reject);
+      server.on("message", (message) => {
+        if (message === "ok") {
           resolve();
         }
       });
@@ -34,26 +33,26 @@ describe('Credit Card Validator form', () => {
     server.kill();
   });
 
-  test('should add do something', async () => {
+  test("should add do something", async () => {
     await page.goto(baseUrl);
   });
 
-  test('should determine paysystem', async () => {
+  test("should determine paysystem", async () => {
     await page.goto(baseUrl);
-    const input = await page.$('#card_number');
-    await input.type('6011');
+    const input = await page.$("#card_number");
+    await input.type("6011");
 
-    await page.$('.discover:not(.inactive)');
-    await page.$('.visa.inactive');
+    await page.$(".discover:not(.inactive)");
+    await page.$(".visa.inactive");
   });
 
-  test('should check test luna', async () => {
+  test("should check test luna", async () => {
     await page.goto(baseUrl);
-    const input = await page.$('#card_number');
-    const submit = await page.$('.submit');
-    await input.type('5572427015897327');
+    const input = await page.$("#card_number");
+    const submit = await page.$(".submit");
+    await input.type("5572427015897327");
 
     await submit.click();
-    await page.$('.validation-success');
+    await page.$(".validation-success");
   });
 });
